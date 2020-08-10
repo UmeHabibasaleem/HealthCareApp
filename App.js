8ed2114d7d44077aed89497cf801a15e2afae4c3
UmeHabibasaleem <umehabiba.cs@gmail.com>
2020-08-10 14:11:40 +0500

    all files

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import TabApp from './Screens/Components/TabNavigation';
+import 'react-native-gesture-handler';
+
+
+const App = () => {
+  return (
+    <>
+    <TabApp />
+    </>
+  );
+};
+
+
+
+export default App;
