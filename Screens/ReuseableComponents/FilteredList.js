f77fa67dd1dfa2f8df5db99816baaf6dacac96cc
UmeHabibasaleem <umehabiba.cs@gmail.com>
2020-08-12 11:01:27 +0500

    color of selected chip is now start to change

--- Screens/ReuseableComponents/FilteredList.js
+++ Screens/ReuseableComponents/FilteredList.js
@@ -1,56 +1,83 @@
 import React ,{useState} from 'react';
-import {StyleSheet,FlatList} from 'react-native';
-import { Chip } from 'react-native-paper';
+import {StyleSheet,ScrollView} from 'react-native';
+import { Chip} from 'react-native-paper';
 import Mycards from '../Components/cards';
 import ProjectsApi from '../api/ProjectsApi';
-import SelectableChips from 'react-native-chip/SelectableChips'
-
 
 const ListScreen = () => {
 
     const [fetchResponse,result,isLoading] = ProjectsApi();
-    const [Selected, setSelected] = useState(false);
-    const dataSource = [
-        "ALL",
-        "HIPA",
-        "EMR",
-        "HL7 INTEGRATION",
-        "M HEALTH",
-        "SMART ON FHIR"
-        ]
+    const [selectedValue, setSelectedValue] = useState("ALL");
+
+    const Categories = [
+        {
+            key: '1',
+            text: 'ALL',
+        },
+        {
+            key: '2',
+            text: 'HIPA',
+        },
+        {
+            key: '3',
+            text: 'EMR',
+        },
+        {
+            key: '4',
+            text: 'HL7 INTEGRATION',
+        },
+        {
+            key: '5',
+            text: 'M HEALTH',
+        },
+        {
+            key: '6',
+            text: 'SMART ON FHIR',
+        },
+
+    ];
+    const callTwoFuctions =(itemtext)=>{
+        setSelectedValue(itemtext),
+            fetchResponse(itemtext)
+    }
+
     return (<>
-            <FlatList
-        horizontal = {true}
-        showsHorizontalScrollIndicator = {false}
-        data = {dataSource}
-        keyExtractor={(item, index) => 'key'+index}
-        renderItem ={({item}) => {
-            return (
-                    <Chip
-                        key={item.name}
-                        selected = {false}
-                        selectedColor = "red"
-                        height={35}
-                        textStyle={{ color:'white',fontSize: 15 }}
-                        style={{ backgroundColor :'#808080',marginHorizontal:5, marginVertical:10 }}
-                        onPress={() =>{
-                            setSelected(true)
-                            fetchResponse({item})
-                        }}
+        <ScrollView style={{flexDirection: 'row'}}
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}>
+                    {Categories.map(item => {
+                    return (
+                        <Chip
+                            key={item.key}
+                            selected = {false}
+                            height={35}
+                            textStyle={{ color:'white',fontSize: 15 }}
+                            style={[ selectedValue === item.text? styles.afterBackColor:styles.beforeBackColor ] }
+                            onPress={() =>{
+                                callTwoFuctions(item.text)
+                            }}
                         >
-                        {item}
-                    </Chip>
-            );
-        }}
-        />
-        <Mycards result= {result} isLoading= {isLoading}/>
+                            {item.text}
+                        </Chip>
+                    );
+                }
+            )
+            }
+        </ScrollView>
+
+            <Mycards result= {result} isLoading= {isLoading}/>
         </>
     )
 }
 
-
-
-
-
-
+const styles = StyleSheet.create({
+    beforeBackColor: {
+        backgroundColor: '#808080',
+        margin: 5
+    },
+    afterBackColor: {
+        backgroundColor: '#1e90ff',
+        margin: 5
+    },
+});
 export default ListScreen;
--- Screens/api/ProjectsApi.js
+++ Screens/api/ProjectsApi.js
@@ -8,34 +8,20 @@ export default () => {
     const [isLoading, setIsLoading] = useState(true);
 
     const fetchResponse = async (category  ) => {
-
-        var resultCategory;
-        if (typeof (category) === "string" || (typeof (category) === "object"  && (category.item ==='ALL')))
-        {
-            try{
-                const res = await axios.get('http://192.168.8.104:3000/Projects')
-                const temp = res.data.users;
-                setResults(temp);
-                setIsLoading(false);
-            }catch (err) {
-                setErrorMessage('something went wrong');
+        try {
+            const res = await axios.get('http://192.168.8.100:3000/Projects')
+            const temp = res.data.users;
+            if (category === 'ALL') {
+                setResults(temp)
+            } else {
+                const CategoryWise = temp.filter((list) => list.category === category)
+                setResults(CategoryWise);
             }
-        }
-        else
-        {
-            resultCategory = (category.item);
-            try{
-                const res = await axios.get('http://192.168.8.104:3000/Projects')
-                const temp = res.data.users;
-                const CategoryWise = temp.filter( (list) => list.category ===  resultCategory)
-                setResults( CategoryWise);
-                setIsLoading(false);
-            }catch (err) {
-                setErrorMessage('something went wrong');
-            }
+            setIsLoading(false);
+        } catch (err) {
+            setErrorMessage('something went wrong');
         }
     }
-
     useEffect(()=>{
 
         fetchResponse("ALL");
